76701ed0cebf962dcd7aba23e70728c4db3ca7dd
Patryk <me@parpok.xyz>
2023-12-29 22:09:04 +0100

    Initial Commit

---
+++ Dick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dick
+//
+//  Created by Patryk Puciłowski on 29/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dick/DickApp.swift
@@ -0,0 +1,17 @@
+//
+//  DickApp.swift
+//  Dick
+//
+//  Created by Patryk Puciłowski on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
