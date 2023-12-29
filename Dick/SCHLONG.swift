c79838697790d272751729fc4185fd499124a370
Patryk <me@parpok.xyz>
2023-12-29 22:32:41 +0100

    What the fuck am I even doing that

---
+++ Dick/SCHLONG.swift
@@ -0,0 +1,18 @@
+//
+//  SCHLONG.swift
+//  Dick
+//
+//  Created by Patryk Puciłowski on 29/12/2023.
+//
+
+import SwiftUI
+
+struct SCHLONG: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SCHLONG()
+}
